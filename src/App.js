fb5ce2bb08a67a7ffec0c8bef9494acd272674cb
Yaakoubi Mohamed <mohamedyaakoubiweb@gmail.com>
2024-05-15 19:58:45 +0100

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [categories, setCategories] = useState([]);
+  const [nom, setNom] = useState('');
+
+  const getCategories = async () => {
+    const data = await axios.get("http://localhost:5000/api/categories");
+    console.log(data.data);
+    setCategories(data.data);
+  }
+
+  const deleteCategories = async (id) => {
+    await axios.delete(`http://localhost:5000/api/categories/${id}`);
+    getCategories()
+  }
+
+  useEffect(() => {
+    getCategories();
+  }, [])
+
+  const AddCategorie = async () =>{
+    await axios.post("http://localhost:5000/api/categories", {
+      nom: nom
+      })
+      .then((response) =>{
+        console.log(response.data);
+        setNom('');
+        getCategories();
+      })
+      .catch((error) =>{
+        console.error(error);
+      })
+      ;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form className='col-md-4'>
+        <div className='form-group'>
+        <label forName="nom">Nom</label>
+        <input id="nom" name='nom' value={nom} onChange={(e) => setNom(e.target.value)} type="text" className='form-control' placeholder="Category Name" />
+        <button className='btn btn-primary' type="button" onClick={() => AddCategorie()}>Add Category</button>
+        </div>        
+      </form>
+
+      <table className='table table-hover table-striped table-responsive'>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>nom</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.map((category) => (
+            <tr>
+              <td>{category.id}</td>
+              <td>{category.nom}</td>
+              <td>
+                <a className='btn btn-success'>Modifier</a>
+                <a className='btn btn-danger' onClick={() => deleteCategories(category.id)}>Supprimer</a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
